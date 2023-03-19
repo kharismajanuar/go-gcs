fccaee8a9002bed29c2007de285a3a3524549587
Kharisma Januar Muhammad JN <kharisma.januar@gmail.com>
2023-03-19 17:50:38 +0700

    config mysql

--- app/database/mysql.go
+++ app/database/mysql.go
@@ -1,1 +1,26 @@
 package database
+
+import (
+	"fmt"
+	"go-gcs/app/config"
+	"log"
+
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
+)
+
+func InitDB(cfg config.AppConfig) *gorm.DB {
+	connectionString := fmt.Sprintf("%s:%s@tcp(%s:%d)/%s?charset=utf8mb4&parseTime=True&loc=Local",
+		cfg.DB_USERNAME, cfg.DB_PASSWORD, cfg.DB_HOSTNAME, cfg.DB_PORT, cfg.DB_NAME)
+	db, err := gorm.Open(mysql.Open(connectionString), &gorm.Config{})
+
+	if err != nil {
+		log.Println("error connect to DB", err.Error())
+		return nil
+	}
+
+	return db
+}
+
+func InitialMigration(db *gorm.DB) {
+}
