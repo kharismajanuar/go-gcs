61cb15cb4fa4dc4296c89325f97fdb5006401628
Kharisma Januar Muhammad JN <kharisma.januar@gmail.com>
2023-03-19 17:49:06 +0700

    config env

--- app/config/config.go
+++ app/config/config.go
@@ -1,1 +1,68 @@
 package config
+
+import (
+	"log"
+	"os"
+	"strconv"
+
+	"github.com/spf13/viper"
+)
+
+type AppConfig struct {
+	DB_USERNAME string
+	DB_PASSWORD string
+	DB_HOSTNAME string
+	DB_PORT     int
+	DB_NAME     string
+}
+
+func InitConfig() *AppConfig {
+	return ReadEnv()
+}
+
+func ReadEnv() *AppConfig {
+	app := AppConfig{}
+	isRead := true
+
+	if val, found := os.LookupEnv("DBUSER"); found {
+		app.DB_USERNAME = val
+		isRead = false
+	}
+	if val, found := os.LookupEnv("DBPASS"); found {
+		app.DB_PASSWORD = val
+		isRead = false
+	}
+	if val, found := os.LookupEnv("DBHOST"); found {
+		app.DB_HOSTNAME = val
+		isRead = false
+	}
+	if val, found := os.LookupEnv("DBPORT"); found {
+		cnv, _ := strconv.Atoi(val)
+		app.DB_PORT = cnv
+		isRead = false
+	}
+	if val, found := os.LookupEnv("DBNAME"); found {
+		app.DB_NAME = val
+		isRead = false
+	}
+
+	if isRead {
+		viper.AddConfigPath(".")
+		viper.SetConfigName("local")
+		viper.SetConfigType("env")
+
+		err := viper.ReadInConfig()
+		if err != nil {
+			log.Println("error read config : ", err.Error())
+			return nil
+		}
+		app.DB_USERNAME = viper.Get("DB_USERNAME").(string)
+		app.DB_PASSWORD = viper.Get("DB_PASSWORD").(string)
+		app.DB_HOSTNAME = viper.Get("DB_HOSTNAME").(string)
+		app.DB_PORT, _ = strconv.Atoi(viper.Get("DB_PORT").(string))
+		app.DB_NAME = viper.Get("DB_NAME").(string)
+
+	}
+
+	return &app
+}
